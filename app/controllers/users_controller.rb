25a3a733d8617c9ec7e84f254b3b8c3b2e97e4aa
akira.j <cw5981ja@yahoo.co.jp>
2020-02-27 05:31:10 +0000

    subit-modify11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
-  def index
-    @users = User.order(id: :desc)
-  end
   def new
     @user = User.new
-  end
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc)
-    counts(@user)
   end
   def create
     @user = User.new(user_params)
